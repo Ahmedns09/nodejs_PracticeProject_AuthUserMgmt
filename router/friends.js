1b1070e8aa8eb928cdb248bb0bf2f314d20a0e31
Ahmedns09 <anskhzyan@gmail.com>
2025-10-03 15:49:53 +0400

    added code for GET, POSt, PUT and DELETE methods

--- router/friends.js
+++ router/friends.js
@@ -40,19 +40,59 @@ router.get("/:email", (req, res) => {
 // POST request: Add a new friend
 router.post("/", (req, res) => {
   // Update the code here
-  res.send("Yet to be implemented"); //This line is to be replaced with actual return value
+  // Check if email is provided in the request body
+  if (req.body.email) {
+    // Create or update friend's details based on provided email
+    friends[req.body.email] = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      DOB: req.body.DOB,
+    };
+  }
+
+  // Send response indicating user addition
+  res.send("The user " + " " + req.body.firstName + " has been added!");
 });
 
 // PUT request: Update the details of a friend with email id
 router.put("/:email", (req, res) => {
   // Update the code here
-  res.send("Yet to be implemented"); //This line is to be replaced with actual return value
+
+  const email = req.params.email;
+  let friend = friends[email];
+  if (friend) {
+    let DOB = req.body.DOB;
+    let firstName = req.body.firstName;
+    let lastName = req.body.lastName;
+
+    if (DOB) {
+      friend.DOB = DOB;
+    }
+    if (firstName) {
+      friend.firstName = firstName;
+    }
+    if (lastName) {
+      friend.lastName = lastName;
+    }
+
+    friends[email] = friend;
+    res.send(`Friend with email $(email) has been updated.`);
+  } else {
+    res.send("Unable to find friend with email " + email);
+  }
 });
 
 // DELETE request: Delete a friend by email id
 router.delete("/:email", (req, res) => {
   // Update the code here
-  res.send("Yet to be implemented"); //This line is to be replaced with actual return value
+
+  const email = req.params.email;
+  if (friends[email]) {
+    delete friends[email];
+  }
+
+  // Send response confirming deletion of friend
+  res.send(`Friend with email ${email} has been deleted.`);
 });
 
 module.exports = router;
